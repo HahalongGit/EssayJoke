6073f4407e03876cc0d14910e2c3712da7c60132
customView <729665268@qq.com>
2018-01-10 17:54:35 +0800

    Android AsyncTask原理实现分析

--- app/src/main/java/com/lll/essay/joke/MainActivity.java
+++ app/src/main/java/com/lll/essay/joke/MainActivity.java
@@ -1,8 +1,10 @@
 package com.lll.essay.joke;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewOutlineProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,8 +27,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewUtils.inject(this);
-
         tvTextContent.setText("通过注解设置文字12");
+
+        AsyncTask task = new AsyncTask<Void,Void,Void>() {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+            }
+
+            @Override
+            protected void onProgressUpdate(Void... values) {
+                super.onProgressUpdate(values);
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+        };
+        task.execute();
     }
 
     @CheckNet
